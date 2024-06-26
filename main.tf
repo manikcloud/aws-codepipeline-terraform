5c6becc845fb867ec14dc8798fcfce4d1520e052
varun <v@gmail.com>
2024-06-26 09:40:09 +0530

    adding tf files

--- main.tf
+++ main.tf
@@ -172,3 +172,80 @@ resource "aws_iam_role_policy_attachment" "codepipeline_policy_attachment" {
 output "codepipeline_role_arn" {
   value = aws_iam_role.codepipeline_role.arn
 }
+
+
+###### codepipeline ################
+
+resource "aws_codepipeline" "pipeline" {
+  name     = "my-codepipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    type     = "S3"
+    location = aws_s3_bucket.codebuild_bucket.bucket
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "S3"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        S3Bucket = aws_s3_bucket.codebuild_bucket.bucket
+        S3ObjectKey = "source.zip" # Update this as needed
+        PollForSourceChanges = "true"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.codebuild_project.name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name             = "Deploy"
+      category         = "Deploy"
+      owner            = "AWS"
+      provider         = "ElasticBeanstalk"
+      version          = "1"
+      input_artifacts  = ["build_output"]
+
+      configuration = {
+        ApplicationName = "my-ebs-application" # Replace with your EBS application name
+        EnvironmentName = "my-ebs-environment" # Replace with your EBS environment name
+      }
+    }
+  }
+
+  tags = {
+    Name        = "CodePipeline"
+    Environment = "Dev"
+  }
+}
+
+output "pipeline_name" {
+  value = aws_codepipeline.pipeline.name
+}
