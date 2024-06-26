9e9c427c4f8429bfa05f457d7ce8e09e11e939ed
varun <v@gmail.com>
2024-06-26 09:33:37 +0530

    adding tf files

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" # Change to your desired region
 }
 
-resource "aws_s3_bucket" "codebuild_bucket" {
+resource "aws_s3_bucket_acl" "codebuild_bucket" {
   bucket = "my-codebuild-bucket-12345" # Change to your desired bucket name
   acl    = "private"
   
@@ -66,3 +66,42 @@ output "codebuild_role_arn" {
   value = aws_iam_role.codebuild_role.arn
 }
 
+########### Code Build ##########
+
+
+resource "aws_codebuild_project" "codebuild_project" {
+  name          = "my-codebuild-project"
+  description   = "My CodeBuild project"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "S3"
+    location = aws_s3_bucket.codebuild_bucket.bucket
+    packaging = "ZIP"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/standard:4.0"
+    type         = "LINUX_CONTAINER"
+  }
+
+  source {
+    type      = "S3"
+    location  = "${aws_s3_bucket.codebuild_bucket.bucket}/source.zip" # Update this as needed
+  }
+
+  cache {
+    type = "S3"
+    location = aws_s3_bucket.codebuild_bucket.bucket
+  }
+
+  tags = {
+    Name        = "CodeBuildProject"
+    Environment = "Dev"
+  }
+}
+
+output "codebuild_project_name" {
+  value = aws_codebuild_project.codebuild_project.name
+}
